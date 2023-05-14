5ce469a5976325636914248702faccb042592c53
kirubelKS <kirubel.n.gebeyehu@gmail.com>
2023-05-14 19:16:45 +0300

    correction on task 2

--- tests/test_console.py
+++ tests/test_console.py
@@ -14,6 +14,7 @@ Unittest classes:
 import os
 import sys
 import unittest
+from models import storage
 from models.engine.file_storage import FileStorage
 from console import HBNBCommand
 from io import StringIO
